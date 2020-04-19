fbe06a45d4ffc030db1bfd8f8e2765dd94342c0d
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-04-19 18:32:33 +0530

    Resolve Bean Conflict with @Primary

---
+++ spring-core/src/main/java/com/ssp/aaps/springcore/dao/DataSource.java
@@ -0,0 +1,6 @@
+package com.ssp.aaps.springcore.dao;
+
+public interface DataSource {
+
+    void getConnection();
+}
--- spring-core/src/main/java/com/ssp/aaps/springcore/dao/UserDao.java
+++ spring-core/src/main/java/com/ssp/aaps/springcore/dao/UserDao.java
@@ -1,9 +1,13 @@
 package com.ssp.aaps.springcore.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao {
+
+  @Autowired
+  private DataSource dataSource;
 
   public void createUser() {
     System.out.println("== >> UserDao: createUser");
