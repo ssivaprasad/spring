c88e080cc094a168bee78db3b3a5818687256dbf
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-04-19 18:42:44 +0530

    Autowire Multiple Beans of Same Type into Collection

--- spring-core/src/main/java/com/ssp/aaps/springcore/dao/UserDao.java
+++ spring-core/src/main/java/com/ssp/aaps/springcore/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.ssp.aaps.springcore.dao;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,10 @@ public class UserDao {
   @Autowired
   @Qualifier("testDataSource")
   private DataSource dataSource;
+
+
+  @Autowired
+  private List<DataSource> dataSources;
 
   public void createUser() {
     System.out.println("== >> UserDao: createUser");
