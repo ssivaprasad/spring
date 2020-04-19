e9be5e6de2e720c418802433a164f2081c0018df
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-04-19 18:36:27 +0530

    Resolve Bean Conflict with @Qualifier

--- spring-core/src/main/java/com/ssp/aaps/springcore/dao/UserDao.java
+++ spring-core/src/main/java/com/ssp/aaps/springcore/dao/UserDao.java
@@ -1,12 +1,14 @@
 package com.ssp.aaps.springcore.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao {
 
   @Autowired
+  @Qualifier("testDataSource")
   private DataSource dataSource;
 
   public void createUser() {
