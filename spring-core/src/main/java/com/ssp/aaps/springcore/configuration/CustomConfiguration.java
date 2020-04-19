e9b800e74aed6d9272993333afaabc21802bd4ce
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-04-19 18:40:29 +0530

    Importing Configuration: @Import

--- spring-core/src/main/java/com/ssp/aaps/springcore/configuration/CustomConfiguration.java
+++ spring-core/src/main/java/com/ssp/aaps/springcore/configuration/CustomConfiguration.java
@@ -1,13 +1,13 @@
 package com.ssp.aaps.springcore.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import com.ssp.aaps.springcore.dao.DataSource;
 import com.ssp.aaps.springcore.dao.UatDataSource;
 
 @Configuration
-@ComponentScan("com.ssp.aaps.springcore")
+@Import({DaoConfiguration.class, ServiceConfiguration.class, ControllerConfiguration.class})
 public class CustomConfiguration {
 
   @Bean
---
+++ spring-core/src/main/java/com/ssp/aaps/springcore/configuration/ServiceConfiguration.java
@@ -0,0 +1,10 @@
+package com.ssp.aaps.springcore.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ssp.aaps.springcore.service")
+public class ServiceConfiguration {
+
+}
