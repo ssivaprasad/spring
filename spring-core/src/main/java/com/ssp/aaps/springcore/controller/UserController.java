2d4f5f2c42ed5cc43ebb60db012eb69062707097
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-04-19 18:17:44 +0530

    Constuctor Based Injection

--- spring-core/src/main/java/com/ssp/aaps/springcore/controller/UserController.java
+++ spring-core/src/main/java/com/ssp/aaps/springcore/controller/UserController.java
@@ -10,9 +10,15 @@ public class UserController {
   private UserService userService;
 
   @Autowired
-  public void setUserService(UserService userService) {
+  public UserController(UserService userService) {
     this.userService = userService;
   }
+
+
+  /*
+   * @Autowired public void setUserService(UserService userService) { this.userService =
+   * userService; }
+   */
 
   public void createuser() {
     userService.saveUser();
