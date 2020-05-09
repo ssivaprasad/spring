dab123e111941df1d9021609916eda302ea2f3dc
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-09 20:37:16 +0530

    Modify Entity

--- thymeleaf/src/main/java/com/ssp/apps/thymeleaf/entity/User.java
+++ thymeleaf/src/main/java/com/ssp/apps/thymeleaf/entity/User.java
@@ -1,21 +1,18 @@
 package com.ssp.apps.thymeleaf.entity;
 
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.GenericGenerator;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import java.util.Date;
-import java.util.UUID;
 
 @Getter
 @Setter
@@ -29,8 +26,13 @@ public class User {
     @Id
     private String id;
 
+    @Column(name = "first_name", unique = true)
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "age")
     private Integer age;
 
     @Temporal(TemporalType.DATE)
