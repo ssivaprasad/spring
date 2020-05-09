946328a3b8ff463fed72da5c6c8ad95f5dfc9500
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-09 19:55:58 +0530

    Add More Dump Data

--- thymeleaf/src/main/resources/data.sql
+++ thymeleaf/src/main/resources/data.sql
@@ -1,2 +1,5 @@
-insert into USER (id, first_name, last_name, age, date_of_birth) values ('ea7694e8-f1c1-4a13-9806-b6f7a2e1f82d', 'Siva', 'Somarouthu', 31, current_date);
-insert into USER (id, first_name, last_name, age, date_of_birth) values ('cc4d9d00-6afc-457b-9bb7-580a0d68db2e', 'Prasad', 'Somarouthu', 31, current_date);
+insert into USER (id, first_name, last_name, age, date_of_birth) values ('cc4d9d00-6afc-457b-9bb7-580a0d68db20', 'Siva', 'Somarouthu', 31, current_date);
+insert into USER (id, first_name, last_name, age, date_of_birth) values ('cc4d9d00-6afc-457b-9bb7-580a0d68db21', 'Prasad', 'Somarouthu', 31, current_date);
+insert into USER (id, first_name, last_name, age, date_of_birth) values ('cc4d9d00-6afc-457b-9bb7-580a0d68db22', 'Atom', 'Somarouthu', 31, current_date);
+insert into USER (id, first_name, last_name, age, date_of_birth) values ('cc4d9d00-6afc-457b-9bb7-580a0d68db23', 'Tim', 'Somarouthu', 31, current_date);
+insert into USER (id, first_name, last_name, age, date_of_birth) values ('cc4d9d00-6afc-457b-9bb7-580a0d68db24', 'Boris', 'Somarouthu', 31, current_date);
