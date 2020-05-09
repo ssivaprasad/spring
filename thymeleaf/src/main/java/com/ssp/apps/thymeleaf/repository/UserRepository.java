af83a33f27064bb56ccd3c43c9fc93da3c75e3c6
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-09 20:32:45 +0530

    Implement CRUD Operations

--- thymeleaf/src/main/java/com/ssp/apps/thymeleaf/repository/UserRepository.java
+++ thymeleaf/src/main/java/com/ssp/apps/thymeleaf/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.ssp.apps.thymeleaf.repository;
 
-import com.ssp.apps.thymeleaf.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.ssp.apps.thymeleaf.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByFirstName(String firstName);
+
 }
--- thymeleaf/src/main/java/com/ssp/apps/thymeleaf/service/UserService.java
+++ thymeleaf/src/main/java/com/ssp/apps/thymeleaf/service/UserService.java
@@ -1,11 +1,11 @@
 package com.ssp.apps.thymeleaf.service;
 
-import com.ssp.apps.thymeleaf.entity.User;
-import com.ssp.apps.thymeleaf.repository.UserRepository;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import com.ssp.apps.thymeleaf.entity.User;
+import com.ssp.apps.thymeleaf.repository.UserRepository;
 
 @Service
 public class UserService {
@@ -13,7 +13,33 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public User getUser(String userId) {
+        return userRepository.findById(userId).map(user -> user)
+                .orElseThrow(() -> new RuntimeException("No User found with id: " + userId));
+    }
+
+    public void createUser(User user) {
+        userRepository.findByFirstName(user.getFirstName()).ifPresent(tempUser -> {
+            throw new RuntimeException("User already Exists");
+        });
+
+        user.setId(UUID.randomUUID().toString());
+        userRepository.save(user);
+    }
+
+    public void updateUser(User user) {
+        userRepository.findById(user.getId())
+                .orElseThrow(() -> new RuntimeException("No User found with id: " + user.getId()));
+        userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("No User found with id: " + userId));
+        userRepository.deleteById(userId);
     }
 }
