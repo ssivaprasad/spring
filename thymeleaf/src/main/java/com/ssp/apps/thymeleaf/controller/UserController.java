71e7bd078ca579c17f67b58d5fe21a9177be05c3
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-09 20:42:09 +0530

    Implement Dynamic Links

--- thymeleaf/src/main/java/com/ssp/apps/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/ssp/apps/thymeleaf/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.ssp.apps.thymeleaf.controller;
 
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.ssp.apps.thymeleaf.dto.UserDto;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.ssp.apps.thymeleaf.entity.User;
 import com.ssp.apps.thymeleaf.mapper.UserMapper;
 import com.ssp.apps.thymeleaf.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -26,17 +26,16 @@ public class UserController {
     @GetMapping
     public String getAllUsers(ModelMap model) {
         List<User> users = userService.getAllUsers();
-
         model.addAttribute("users", userMapper.conver2UserDtoList(users));
         return "user-list";
     }
 
 
     @GetMapping("view-user")
-    public String viewUserDetails(ModelMap model) {
-        UserDto userDto =
-                new UserDto(UUID.randomUUID().toString(), "Siva", "Somarouthu", 31, new Date());
-        model.addAttribute("user", userDto);
+    public String viewUserDetails(ModelMap model, @RequestParam(name = "id") String userId) {
+        log.info("== >> UserId: {}", userId);
+        User user = userService.getUser(userId);
+        model.addAttribute("user", userMapper.convert2Dto(user));
         return "view-user";
     }
 }
