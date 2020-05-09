9036a24dca653393d7bbd493af62ed40a33f940e
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-09 19:20:35 +0530

    Custom Mapper

--- thymeleaf/src/main/java/com/ssp/apps/thymeleaf/mapper/UserMapper.java
+++ thymeleaf/src/main/java/com/ssp/apps/thymeleaf/mapper/UserMapper.java
@@ -1,19 +1,27 @@
 package com.ssp.apps.thymeleaf.mapper;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
 import com.ssp.apps.thymeleaf.dto.UserDto;
 import com.ssp.apps.thymeleaf.entity.User;
-import org.mapstruct.Mapper;
 
-import java.util.List;
+@Component
+public class UserMapper {
 
-@Mapper
-public interface UserMapper {
+    public UserDto convert2Dto(User user) {
+        return new UserDto(user.getId(), user.getFirstName(), user.getLastName(), user.getAge(),
+                user.getDateOfBirth());
+    }
 
-    UserDto convert2Dto(User user);
+    public User convert2Entity(UserDto userDto) {
+        return new User(userDto.getId(), userDto.getFirstName(), userDto.getLastName(),
+                userDto.getAge(), userDto.getDateOfBirth());
+    }
 
-    User convert2Entity(UserDto userDto);
-
-    List<UserDto> conver2UserDtoList(List<User> users);
+    public List<UserDto> conver2UserDtoList(List<User> users) {
+        return users.stream().map(user -> convert2Dto(user)).collect(Collectors.toList());
+    }
 
 }
