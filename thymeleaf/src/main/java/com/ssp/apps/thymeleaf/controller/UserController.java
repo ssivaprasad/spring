114ea424e0f0f4fca48c32e63bb5473bbaff1c57
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-09 20:05:20 +0530

    looping through Collections

--- thymeleaf/src/main/java/com/ssp/apps/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/ssp/apps/thymeleaf/controller/UserController.java
@@ -1,18 +1,17 @@
 package com.ssp.apps.thymeleaf.controller;
 
-import com.ssp.apps.thymeleaf.dto.UserDto;
-import com.ssp.apps.thymeleaf.entity.User;
-import com.ssp.apps.thymeleaf.mapper.UserMapper;
-import com.ssp.apps.thymeleaf.service.UserService;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import com.ssp.apps.thymeleaf.dto.UserDto;
+import com.ssp.apps.thymeleaf.entity.User;
+import com.ssp.apps.thymeleaf.mapper.UserMapper;
+import com.ssp.apps.thymeleaf.service.UserService;
 
 @Controller
 @RequestMapping("/users")
@@ -25,8 +24,9 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping
-    public String getAllUsers(ModelMap model){
+    public String getAllUsers(ModelMap model) {
         List<User> users = userService.getAllUsers();
+
         model.addAttribute("users", userMapper.conver2UserDtoList(users));
         return "user-list";
     }
@@ -34,7 +34,8 @@ public class UserController {
 
     @GetMapping("view-user")
     public String viewUserDetails(ModelMap model) {
-        UserDto userDto = new UserDto(UUID.randomUUID().toString(), "Siva", "Somarouthu", 31, new Date());
+        UserDto userDto =
+                new UserDto(UUID.randomUUID().toString(), "Siva", "Somarouthu", 31, new Date());
         model.addAttribute("user", userDto);
         return "view-user";
     }
