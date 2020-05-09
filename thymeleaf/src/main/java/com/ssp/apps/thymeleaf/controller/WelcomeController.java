0a554cc0fee1814df1d7122111fccf2e994139aa
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-09 19:55:42 +0530

    create home page

--- thymeleaf/src/main/java/com/ssp/apps/thymeleaf/controller/WelcomeController.java
+++ thymeleaf/src/main/java/com/ssp/apps/thymeleaf/controller/WelcomeController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
 
     @RequestMapping("/")
+    public String home(ModelMap map) {
+        return "home";
+    }
+
+    @RequestMapping("/welcome")
     public String welome(ModelMap map) {
         map.addAttribute("userName", "Siva Prasad");
         return "welcome";
